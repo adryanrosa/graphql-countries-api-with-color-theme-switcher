8ae3b73bf013857399447d4c8aac91b2650796eb
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-06 16:47:36 -0300

    Corrige erro deploy

--- context/Provider.jsx
+++ context/Provider.jsx
@@ -20,13 +20,13 @@ function Provider({ children }) {
     const variables = {
       lightMode: {
         text: 'hsl(200, 15%, 8%)',
-        elements: 'hsl(0, 0%, 100%)',
+        element: 'hsl(0, 0%, 100%)',
         background: 'hsl(0, 0%, 98%)',
         input: 'hsl(0, 0%, 52%)',
       },
       darkMode: {
         text: 'hsl(0, 0%, 100%)',
-        elements: 'hsl(209, 23%, 22%)',
+        element: 'hsl(209, 23%, 22%)',
         background: 'hsl(207, 26%, 17%)',
         input: 'hsl(0, 0%, 100%)',
       },
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  // eslint-disable-next-line no-magic-numbers
   res.status(200).json({ name: 'John Doe' });
 }
