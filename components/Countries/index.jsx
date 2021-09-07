d1d138d324d618b5f7ce78f1d676fedd83c44bbe
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-07 17:56:39 -0300

    Ajusta padding de Countries

--- components/Countries/index.jsx
+++ components/Countries/index.jsx
@@ -2,10 +2,12 @@ import Country from './Country';
 
 function Countries({ countries }) {
   return (
-    <main className="countries container">
-      { countries.map(({ node }) => (
-        <Country key={ node.numericCode } country={ node } />
-      ))}
+    <main className="countries">
+      <div className="countries__layout container">
+        { countries.map(({ node }) => (
+          <Country key={ node.numericCode } country={ node } />
+        ))}
+      </div>
     </main>
   );
 }
